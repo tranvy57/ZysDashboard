0ad0df596c61e4cceb9896f057948a74869fd30f
tranvy57 <tranvy.art@gmail.com>
2023-08-03 19:33:37 +0700

    add not-found page

---
+++ src/app/[...not-found]/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+	return <div className="bg-green-300">NotFoundPage</div>
+}
+
+export default NotFoundPage
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const NotFound = () => {
+	return <div className="bg-red-300">not-found</div>
+}
+
+export default NotFound
